0299e0faee517f8afe558745cc00fda0391cd9a4
sdll <shura.sdll@gmail.com>
2019-02-16 23:41:11 +0300

    add the boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import "./App.css";
+import Map from "./components/Map";
+
+class App extends React.Component {
+  public render() {
+    return <Map />;
+  }
+}
+
+export default App;
