3889f9151aba68adb71e9e35c2da3d62f3225608
sdll <sasha@sdll.space>
2019-04-06 16:02:13 +0300

    fix: handle registered users smarter

--- src/service/lambda/subscribe.ts
+++ src/service/lambda/subscribe.ts
@@ -1,12 +1,14 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
 import Axios, { AxiosError, AxiosResponse } from "axios";
 import * as dotenv from "dotenv";
+import md5 from "md5";
 
 dotenv.config();
 
-const mailChimpAPI = process.env.REACT_APP_MAILCHIMP_API_KEY;
-const mailChimpListID = process.env.REACT_APP_MAILCHIMP_LIST_KEY;
-const mcRegion = process.env.REACT_APP_MAILCHIMP_REGION;
+const mailChimpAPI = process.env.REACT_APP_MAILCHIMP_API_KEY || "";
+const mailChimpListID = process.env.REACT_APP_MAILCHIMP_LIST_KEY || "";
+const mcRegion = process.env.REACT_APP_MAILCHIMP_REGION || "";
+const mailChimpURL = `https://${mcRegion}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members`;
 
 export const handler: Handler = (
   event: APIGatewayEvent,
@@ -51,38 +53,36 @@ export const handler: Handler = (
   const subscriber = JSON.stringify(data);
   console.log("Sending the following data to mailchimp: ", subscriber);
 
-  Axios.post(
-    `https://${mcRegion}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members`,
-    data,
-    config
-  )
-    .then((res: AxiosResponse) => {
-      console.log("Mailchimp body: " + JSON.stringify(res.data));
-      console.log("Status Code: " + res.status);
-      if (res.status < 300) {
-        console.log("Subscribed to the mailing list");
-        callback(null, {
-          statusCode: 201,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": "true"
-          },
-          body: JSON.stringify({
-            status: "saved email",
-            params
-          })
+  Axios.get(`${mailChimpURL}/${md5(email)}`, config)
+    .then(res => {
+      if (res.data.status === "unsubscribed") {
+        Axios.patch(
+          `${mailChimpURL}/${md5(email)}`,
+          { status: "pending" },
+          config
+        ).then((patchResponse: AxiosResponse) => {
+          console.log("Mailchimp body: " + JSON.stringify(patchResponse.data));
+          console.log("Status Code: " + patchResponse.status);
+          if (patchResponse.status < 300) {
+            console.log("Subscribed to the mailing list");
+            callback(null, {
+              statusCode: 201,
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"
+              },
+              body: JSON.stringify({
+                status: "saved email",
+                params
+              })
+            });
+          } else {
+            console.log("Error from mailchimp", patchResponse.data.detail);
+            callback(patchResponse.data.detail);
+          }
         });
       } else {
-        console.log("Error from mailchimp", res.data.detail);
-        callback(res.data.detail);
-      }
-    })
-    .catch((err: AxiosError) => {
-      if (
-        err.response.data.status === 400 &&
-        err.response.data.title === "Member Exists"
-      ) {
         callback(null, {
           statusCode: 400,
           headers: {
@@ -92,8 +92,50 @@ export const handler: Handler = (
           },
           body: "subscribe.errors.userExists"
         });
-      } else {
-        callback(err);
       }
+    })
+    .catch(() => {
+      Axios.post(mailChimpURL, data, config)
+        .then((res: AxiosResponse) => {
+          console.log("Mailchimp body: " + JSON.stringify(res.data));
+          console.log("Status Code: " + res.status);
+          if (res.status < 300) {
+            console.log("Subscribed to the mailing list");
+            callback(null, {
+              statusCode: 201,
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"
+              },
+              body: JSON.stringify({
+                status: "saved email",
+                params
+              })
+            });
+          } else {
+            console.log("Error from mailchimp", res.data.detail);
+            callback(res.data.detail);
+          }
+        })
+        .catch((err: AxiosError) => {
+          if (
+            err.response.data.status === 400 &&
+            err.response.data.title === "Member Exists"
+          ) {
+            console.log(err);
+            callback(null, {
+              statusCode: 400,
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"
+              },
+              body: "subscribe.errors.userExists"
+            });
+          } else {
+            callback(err);
+          }
+        });
     });
 };
