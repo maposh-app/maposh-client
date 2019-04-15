79cd34f47d8c4a2feca0afe5d63e6b05489cc127
sdll <sasha@sdll.space>
2019-04-15 23:12:40 +0300

    feat(auth): add facebook login

---
+++ src/containers/login/facebook/ index.tsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { FacebookButton } from "./facebook.css";
+import { Auth } from "aws-amplify";
+
+interface AuthWindow extends Window {
+  FB: any;
+}
+
+interface IFacebookResponse {
+  status: string;
+  email?: string;
+  public_profile?: { name: string };
+  accessToken?: string;
+  expiresIn?: number;
+}
+export const FacebookLogin: React.FC = () => {
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [timer, setTimer] = React.useState(0);
+  React.useEffect(() => {
+    initFacebook();
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  const checkFacebook = React.useCallback(() => {
+    return new Promise((res, rej) => {
+      const hasFbLoaded = () => {
+        if ((window as AuthWindow).FB) {
+          res();
+        } else {
+          setTimer(setTimeout(hasFbLoaded, 300));
+        }
+      };
+      hasFbLoaded();
+    });
+  }, []);
+
+  const initFacebook = React.useCallback(async () => {
+    await checkFacebook();
+    setIsLoading(false);
+  }, []);
+
+  const handleError = React.useCallback((error: any) => {
+    alert(error);
+  }, []);
+
+  const handleResponse = React.useCallback(async (data: IFacebookResponse) => {
+    const { email, public_profile, accessToken: token, expiresIn } = data;
+    const expiresAt = expiresIn ? expiresIn * 1000 + new Date().getTime() : 0;
+    const user = { email, name: public_profile ? public_profile.name : "" };
+
+    setIsLoading(true);
+
+    try {
+      await Auth.federatedSignIn(
+        "facebook",
+        { token: token as string, expires_at: expiresAt },
+        user
+      );
+    } catch (e) {
+      handleError(e);
+    }
+    setIsLoading(false);
+  }, []);
+
+  const saveStatus = React.useCallback((response: IFacebookResponse) => {
+    if (response.status === "connected") {
+      handleResponse(response);
+    } else {
+      handleError(response);
+    }
+  }, []);
+
+  const checkStatus = React.useCallback(
+    () => (window as AuthWindow).FB.getLoginStatus(saveStatus),
+    []
+  );
+
+  const handleClick = React.useCallback(() => {
+    (window as AuthWindow).FB.login(checkStatus, {
+      scope: "public_profile,email"
+    });
+  }, []);
+  return (
+    <FacebookButton disabled={!isLoading} onClick={handleClick}>
+      {t("login.facebook")}
+    </FacebookButton>
+  );
+};
--- src/service/store/bridges/amplify.ts
+++ src/service/store/bridges/amplify.ts
@@ -1,15 +1,50 @@
 import { Auth, Hub } from "aws-amplify";
 import MaposhStore from "..";
 import { updateAuthStatus } from "../system/actions";
+import config from "../../../config";
+
+interface AuthWindow extends Window {
+  fbAsyncInit: () => any;
+  FB: any;
+}
 
 export default class AmplifyBridge {
+  private loadFacebookSDK() {
+    (window as AuthWindow).fbAsyncInit = () => {
+      (window as AuthWindow).FB.init({
+        appId: process.env.REACT_APP_FACEBOOK_APP_ID,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v3.1"
+      });
+    };
+
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        console.log("already exist");
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      (js as HTMLScriptElement).src = config.auth.facebook.sdk_url;
+      if (fjs.parentNode) {
+        fjs.parentNode.insertBefore(js, fjs);
+        console.log("attaching");
+      }
+    })(document, "script", "facebook-jssdk");
+  }
+
   constructor() {
     this.onHubCapsule = this.onHubCapsule.bind(this);
-    Hub.listen("auth", this, "AmplifyBridge");
+    Hub.listen("auth", this.onHubCapsule);
     this.checkUser();
   }
 
   public onHubCapsule() {
+    console.log("sending");
+    this.loadFacebookSDK();
     this.checkUser();
   }
 
